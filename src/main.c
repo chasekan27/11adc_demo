73490482ad60804090b510d3129209d05af50c6f
chasekan27 <chasekan27@gmail.com>
2025-12-22 01:05:03 +0530

    build success but it is not showing the right adc value

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ static const int32_t sleep_time = 1000;
 //get device tree configurations
 #define MY_ADC_CH DT_ALIAS(myadcchannel)
 static const struct device *adc = DEVICE_DT_GET(DT_ALIAS(myadc));
-static struct adc_channel_cfg adc_ch = ADC_CHANNEL_CFG_DT(MY_ADC_CH);
+static const struct adc_channel_cfg adc_ch = ADC_CHANNEL_CFG_DT(MY_ADC_CH);
 int main(void)
 {
     int ret;
@@ -41,7 +41,7 @@ int main(void)
             continue;
         }
         val_mv = buf;
-        int32_t voltage_mv = (val_mv * vref_mv) / ((1 << seq.resolution) - 1);
+        int32_t voltage_mv = val_mv * vref_mv / (1 << seq.resolution);
 
         printf("ADC Channel %d: Raw Value = %d, Voltage = %d mV\n",
                adc_ch.channel_id, val_mv, voltage_mv);
